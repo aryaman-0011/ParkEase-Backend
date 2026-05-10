cddf16497f040ddf41355ab28c6d546e1e6a6fd2
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-05-10 15:01:17 +0530

    [Aryaman]: fix gateway test for CI environment

--- api-gateway/src/test/java/com/parkease/gateway/GatewayApplicationTests.java
+++ api-gateway/src/test/java/com/parkease/gateway/GatewayApplicationTests.java
@@ -3,7 +3,12 @@ package com.parkease.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"app.jwt.secret=dGVzdC1zZWNyZXQta2V5LWZvci1jaS1waXBlbGluZS1vbmx5",
+	"spring.cloud.gateway.routes[0].id=test",
+	"spring.cloud.gateway.routes[0].uri=http://localhost:8080",
+	"spring.cloud.gateway.routes[0].predicates[0]=Path=/test/**"
+})
 class GatewayApplicationTests {
 
 	@Test
